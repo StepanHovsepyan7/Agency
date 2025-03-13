b4955af7bc63319503aee2bac29916a7f80621c1
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-03-13 22:48:09 +0400

    Fixed my dark and light mode bug

--- js/script.js
+++ js/script.js
@@ -159,177 +159,22 @@ $(function(){
 
 
 
-    let moon = $('.fa-moon') 
+let moon = $('.fa-moon');
+let darkMode = false;
 
-    moon.click(function(){
-        $('body').css({
-            backgroundColor: '#212529',
-        })
-        $('.parentServices h1').css({
-            color: 'white',
-        })
-        $('.modalParent').css({
-            backgroundColor: '#212529',
-        })
-        $('.modalParent2').css({
-            backgroundColor: '#212529',
-        })
-        $('.parentServices2 h3').css({
-            color: 'white',
-        })
-        $('.portfolioDiv1 h1').css({
-            color: 'white',
-        })
-        $('.modalParent h1').css({
-            color: 'white',
-        })
-        $('.modalParent2 h1').css({
-            color: 'white',
-        })
-        $('.modalParent3').css({
-            backgroundColor: '#212529',
-        })
-        $('.modalParent4 ').css({
-            backgroundColor: '#212529',
-        })
-        $('.modalParent5').css({
-            backgroundColor: '#212529',
-        })
-        $('.modalParent6').css({
-            backgroundColor: '#212529',
-        })
-        $('.modalParent3 h1').css({
-            color: 'white',
-        })
-        $('.modalParent4 h1').css({
-            color: 'white',
-        })
-        $('.modalParent5 h1').css({
-            color: 'white',
-        })
-        $('.modalParent6 h1').css({
-            color: 'white',
-        })
-        $('.imgDivs').css({
-            backgroundColor: 'black ',
-        })
-        $('.imgDivs h4').css({
-            color: 'white',
-        })
-        $('.aboutParent1 h1').css({
-            color: 'white',
-        })
-        $('.div2 h3').css({
-            color: 'white',
-        })
-        $('.div3 h3').css({
-            color: 'white',
-        })
-        $('.teamH1').css({
-            color: 'white',
-        })
-        $('.parentTeam h4').css({
-            color: 'white',
-        })
-        $('.parentTeam p').css({
-            color: 'white',
-        })
-        $('.fa-twitter').css({
-            color: 'white',
-        })
-        $('.fa-facebook').css({
-            color: 'white',
-        })
-        $('.fa-linkedin').css({
-            color: 'white',
-        })
+moon.click(function () {
+    darkMode = !darkMode; // Toggle mode
 
+    if (darkMode) {
+        $('body').css({ backgroundColor: '#212529' });
+        $('.parentServices h1, .parentServices2 h3, .portfolioDiv1 h1, .modalParent h1, .modalParent2 h1, .modalParent3 h1, .modalParent4 h1, .modalParent5 h1, .modalParent6 h1, .imgDivs h4, .aboutParent1 h1, .div2 h3, .div3 h3, .teamH1, .parentTeam h4, .parentTeam p, .fa-twitter, .fa-facebook, .fa-linkedin').css({ color: 'white' });
+        $('.modalParent, .modalParent2, .modalParent3, .modalParent4, .modalParent5, .modalParent6, .imgDivs').css({ backgroundColor: '#212529' });
+    } else {
+        $('body').css({ backgroundColor: 'white' });
+        $('.parentServices h1, .parentServices2 h3, .portfolioDiv1 h1, .modalParent h1, .modalParent2 h1, .modalParent3 h1, .modalParent4 h1, .modalParent5 h1, .modalParent6 h1, .imgDivs h4, .aboutParent1 h1, .div2 h3, .div3 h3, .teamH1, .parentTeam h4, .parentTeam p, .fa-twitter, .fa-facebook, .fa-linkedin').css({ color: 'black' });
+        $('.modalParent, .modalParent2, .modalParent3, .modalParent4, .modalParent5, .modalParent6, .imgDivs').css({ backgroundColor: 'white' });
+    }
+});
 
-        moon.click(function(){
-            $('body').css({
-                backgroundColor: 'white',
-            })
-            $('.parentServices h1').css({
-                color: 'black',
-            })
-            $('.parentServices2 h3').css({
-                color: 'black',
-            })
-            $('.portfolioDiv1 h1').css({
-                color: 'black',
-            })
-            $('.imgDivs').css({
-                backgroundColor: 'white ',
-            })
-            $('.imgDivs h4').css({
-                color: 'black',
-            })
-            $('.modalParent').css({
-                backgroundColor: 'white',
-            })
-            $('.modalParent2').css({
-                backgroundColor: 'white',
-            })
-            $('.modalParent3').css({
-                backgroundColor: 'white',
-            })
-            $('.modalParent4').css({
-                backgroundColor: 'white',
-            })
-            $('.modalParent5').css({
-                backgroundColor: 'white',
-            })
-            $('.modalParent6').css({
-                backgroundColor: 'white',
-            })
-            $('.modalParent3 h1').css({
-                color: 'black',
-            })
-            $('.modalParent h1').css({
-                color: 'black',
-            })
-            $('.modalParent2 h1').css({
-                color: 'black',
-            })
-            $('.modalParent4 h1').css({
-                color: 'black',
-            })
-            $('.modalParent5 h1').css({
-                color: 'black',
-            })
-            $('.modalParent6 h1').css({
-                color: 'black',
-            })
-            $('.aboutParent1 h1').css({
-                color: 'black',
-            })
-            $('.div2 h3').css({
-                color: 'black',
-            })
-            $('.div3 h3').css({
-                color: 'black',
-            })
-            $('.fa-twitter').css({
-                color: 'black',
-            })
-            $('.fa-facebook').css({
-                color: 'black',
-            })
-            $('.fa-linkedin').css({
-                color: 'black',
-            })
-            $('.teamH1').css({
-                color: 'black',
-            })
-            $('.parentTeam p').css({
-                color: 'black',
-            })
-            $('.parentTeam h4').css({
-                color: 'black',
-            })
-        })
-
-
-    })
 })
 
